7649aae114f6e97361a20b9d3d442357afec60e4
Taras Shevchuk <shevchukt@mydigicode.com>
2020-07-12 00:17:03 +0300

    Task management

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import * as config from 'config';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const serverConfig = config.get('server');
+  const logger = new Logger('bootstrap');
+  const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requests from origin ${serverConfig.origin}`);
+  }
+
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
+  logger.log(`Application is listening on port ${port}`);
+}
+bootstrap();
